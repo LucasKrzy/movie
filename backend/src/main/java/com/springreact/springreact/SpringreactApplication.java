91ea523b8719174e12566a30d44d1c707282e98e
LucasKrzy <lucaskrzy@gmail.com>
2022-05-13 19:14:27 -0300

    Project created

---
+++ backend/src/main/java/com/springreact/springreact/SpringreactApplication.java
@@ -0,0 +1,13 @@
+package com.springreact.springreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactApplication.class, args);
+	}
+
+}
