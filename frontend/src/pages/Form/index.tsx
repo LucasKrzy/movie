e531111a29f371121996cbebb2c3c71281e451a9
LucasKrzy <lucaskrzy@gmail.com>
2022-05-13 21:12:40 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,43 @@
-function Listing (){
-    return(
-        <h1>Página de formulario</h1>
+import './styles.css';
+
+function Form() {
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+
+    return (
+        <div className="movie-form-container">
+            <img className="movie-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="movie-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="movie-form">
+                    <div className="form-group movie-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group movie-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="movie-form-btn-container">
+                        <button type="submit" className="btn btn-primary movie-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary movie-btn mt-3">Cancelar</button>
+            </div >
+        </div >
     );
 }
-export default Listing;
+export default Form;
