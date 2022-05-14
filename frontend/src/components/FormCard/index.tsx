95f6f3893ad53453825c8824819d3d7744c2c2db
LucasKrzy <lucaskrzy@gmail.com>
2022-05-14 18:52:22 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,9 +1,10 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import axios, { AxiosRequestConfig } from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
 import { BASE_URL } from "utils/request";
-import './styles.css'
+import { validateEmail } from "utils/validate";
+import './styles.css';
 
 type Props = {
     movieId: string;
@@ -11,22 +12,51 @@ type Props = {
 
 function FormCard({ movieId }: Props) {
 
+    const navigate = useNavigate();
+
     const [movie, setMovie] = useState<Movie>();
 
     useEffect(() => {
         axios.get(`${BASE_URL}movies/${movieId}`)
-            .then(respose =>{
+            .then(respose => {
                 setMovie(respose.data);
             }
-        );
+            );
     }, [movieId]);
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+        const score = (event.target as any).score.value;
+
+        if (!validateEmail(email)) {
+            return;
+        }
+        
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            navigate("/");
+        })
+    }
 
     return (
         <div className="movie-form-container">
             <img className="movie-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="movie-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="movie-form">
+                <form className="movie-form" onSubmit={handleSubmit}>
                     <div className="form-group movie-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
